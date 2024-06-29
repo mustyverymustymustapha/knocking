c50029f9d949e4ef1fe164e70174328aa4b264dd
mustyverymustymustapha <mustaphasatti1@gmail.com>
2024-06-29 17:02:51 +0000

    Second Commit

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 
+const jokes = [
+    "Knock, knock. Who's there? Boo. Boo who? Don't cry, it's just a joke!",
+    "Knock, knock. Who's there? Lettuce. Lettuce who? Lettuce in, it's cold out here!",
+    
+];
 
+document.getElementById("get-joke").addEventListener("click", generateJoke);
+
+function generateJoke() {
+    const randomIndex = Math.floor(Math.random() * jokes.length);
+    document.getElementById("joke").textContent = jokes[randomIndex];
+}
